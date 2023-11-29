03b6160ee76cc36a37647207255b90eb3cceb795
Raul Nascimento <raulnascimento.contato@gmail.com>
2023-11-29 11:11:15 -0300

    initial code

---
+++ src/containers/app/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Header} from '../../shared/components/header/header.component';
+
+function App() {
+	return (
+		<div className='App'>
+			<Header />
+		</div>
+	);
+}
+
+export default App;
