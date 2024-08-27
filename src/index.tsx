21f58f9749853e5b0ae8e2ba53101e417f042004
Raul <raulnascimento.contato@gmail.com>
2024-08-27 14:14:15 -0300

    Testing deploy with netlify

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './containers/app/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
-root.render(
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
+} else {
+  console.error('Element with id "root" not found.');
+}
